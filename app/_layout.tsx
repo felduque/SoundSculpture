c2e3c812952191b2a3e7b03625eaafafb249941d
Felipe Duque <111407503+felduque@users.noreply.github.com>
2025-06-28 17:49:16 -0500

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -43,6 +43,7 @@ export default function RootLayout() {
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
         <Stack>
           <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen name="sculpture/[id].tsx" options={{ headerShown: false }} />
           <Stack.Screen name="+not-found" />
         </Stack>
         <StatusBar style="auto" />
